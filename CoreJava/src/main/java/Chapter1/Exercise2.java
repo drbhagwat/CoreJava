b2e3a58c4368f2682bf6e86283d863f36707ce2f
Dinesh <drbhagwat@yahoo.com>
2019-03-12 10:22:47 +0530

    Started solving exercises

---
+++ CoreJava/src/main/java/Chapter1/Exercise2.java
@@ -0,0 +1,24 @@
+package Chapter1;
+
+import java.util.Scanner;
+
+/* 
+ * Write a program that reads an integer angle (which may be positive or 
+ * negative and normalizes it to a value between 0 and 359 degrees. 
+ * Try it first with the % operator, then with floorMod 
+ */
+class Exercise2 {
+  public static void main(String args[]) {
+	System.out.print("Please input a angle: ");
+
+	Scanner scanner = new Scanner(System.in);
+	int angle = scanner.nextInt();
+	scanner.close();
+
+	angle %= 360; // first normalize it between 0 and 359
+	angle = (angle < 0) ? (angle + 360) : angle;
+
+	System.out.printf("The normalzied angle is -> %d\n", angle);
+	System.out.printf("The normalzied angle using Math.floorMod is -> %d\n", Math.floorMod(angle, 360));
+  }
+}
