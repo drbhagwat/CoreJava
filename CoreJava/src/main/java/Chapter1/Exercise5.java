4732ef39cbd0a0b25388700e64d1d21fd8cab161
Dinesh <drbhagwat@yahoo.com>
2019-03-12 22:16:32 +0530

    Completed until Exercise 7

---
+++ CoreJava/src/main/java/Chapter1/Exercise5.java
@@ -0,0 +1,17 @@
+package Chapter1;
+
+
+/* 
+ * What happens when you case a Double to an int that is larger than the 
+ * largest possible int value. Try it out.
+ */
+public class Exercise5 {
+
+  public static void main(String[] args) {
+	int i =  (int) Double.MAX_VALUE;
+	
+	System.out.println("The largest double is " + Double.MAX_VALUE);
+	System.out.println("The value of an int variable, when a double that is larger than the largest possibel int is cast to it, is" + i);
+	System.out.println("The largest int value is " + Integer.MAX_VALUE);
+  }
+}
