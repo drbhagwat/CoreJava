4075c6a8963c63d96d233d9778fca258d22232fa
Dinesh <drbhagwat@yahoo.com>
2019-03-15 20:50:39 +0530

    Done until Exercise 11 of chapter1

--- CoreJava/src/main/java/Chapter1/Exercise9.java
+++ CoreJava/src/main/java/Chapter1/Exercise9.java
@@ -9,15 +9,31 @@ public class Exercise9 {
   public static void main(String[] args) {
 	String string1 = "dinesh";
 	
-	// if you just concatenate two strings and the result happens to be an already defined string
-	// Java is smart enough to refer to the already defined string
-	String string2 = "din" + "esh";
+	/*
+	 * If you concatenate two or more string literals and if the result is exactly the same as 
+	 * an already defined string literal, Java is smart enough to point the reference of the resul to 
+	 * the earlier defined string literal as seen by the following example.
+	 */
 	
-	// here the memory is allocated on heap.
+	String string2 = "d" + "i" + "n" + "e" + "s" + "h";
+	
+	/*
+	 * Here, for string3, the memory is allocated on heap and hence though the content is the same as earlir, 
+	 * the reference is different.
+	*/
+	
 	String string3 = new String("dinesh");
+
+	/*
+	 * Once you internalize string3, the new string would again point to the same earlier defined 
+	 * string reference.
+	 */
+
+	String string4 = string3.intern();
 
 	System.out.println("Is string1 == string2? " + string1.equals(string2));
 	System.out.println("Is string1 refers to the same address as string2? " + (string1 == string2));
 	System.out.println("Is string1 refers to the same address as string3? " + (string1 == string3));
+	System.out.println("Is string1 refers to the same address as string3? " + (string1 == string4));
   }
 }
