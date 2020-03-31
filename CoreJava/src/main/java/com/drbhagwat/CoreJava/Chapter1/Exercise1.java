2496d93d38f2b58e3a0562915ccb4712575a8eb3
bdinesh <bdinesh@s3groupinc.com>
2020-03-31 13:03:02 +0530

    Removed explicit JUnit dependencies...

--- CoreJava/src/main/java/com/drbhagwat/CoreJava/Chapter1/Exercise1.java
+++ CoreJava/src/main/java/com/drbhagwat/CoreJava/Chapter1/Exercise1.java
@@ -3,23 +3,29 @@ package com.drbhagwat.CoreJava.Chapter1;
 import java.util.Scanner;
 
 /*
- * Write a program that reads an integer and prints it in binary,
- * octal and hexadecimal. Print the reciprocal as a hexadecimal
- * floating-point number.
+ * Problem Statement:
+ * Write a program that reads an integer and prints it in binary, octal and hexadecimal. Print the reciprocal as a
+ * hexadecimal floating-point number.
+ */
+
+/**
+ * This program reads an integer and prints it in binary, octal and hexadecimal. It also prints the reciprocal of
+ * the same number in the hexadecimal floating-point format.
  */
 class Exercise1 {
   public static void main(String[] args) {
-    System.out.print("Please input a number: ");
+    System.out.print("Please input an integer: ");
 
     Scanner scanner = new Scanner(System.in);
     int i = scanner.nextInt();
+
+    // As a good practice, close the system resource (when not needed), as they are limited in number
     scanner.close();
 
-    System.out.printf("The number's decimal representation is -> %d,\n" +
-            "binary representation is -> %s,\n" +
+    System.out.printf("The number's binary representation is -> %s,\n" +
             "octal representation is -> %o,\n" +
             "hexadecimal representation is -> %x,\n" +
-            "and its reciprocal in hexadecimal representation is -> %a\n",
-        i, Integer.toBinaryString(i), i, i, 1.0 / i);
+            "and its reciprocal (in hexadecimal representation) is -> %a\n",
+        Integer.toBinaryString(i), i, i, 1.0 / i);
   }
 }
