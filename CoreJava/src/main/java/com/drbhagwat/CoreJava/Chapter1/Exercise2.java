b324b2d8450afa9eda0d45702f621042c176702c
bdinesh <bdinesh@s3groupinc.com>
2020-04-02 11:31:18 +0530

    Worked on Exercise3

--- CoreJava/src/main/java/com/drbhagwat/CoreJava/Chapter1/Exercise2.java
+++ CoreJava/src/main/java/com/drbhagwat/CoreJava/Chapter1/Exercise2.java
@@ -9,7 +9,6 @@ import java.util.Scanner;
  * between 0 and 359 degrees. Try it first with the % operator, then with floorMod.
  */
 
-
 /**
  * This program reads reads an integer angle (which may be positive or negative) and normalizes it to a value
  * between 0 and 359 degrees. First with the % operator, then with floorMod.
