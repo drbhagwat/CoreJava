9fae99e26bea676c70650448c8c73b911f8b50cd
bdinesh <bdinesh@s3groupinc.com>
2020-04-07 14:41:25 +0530

    Worked on Exercise5.

--- CoreJava/src/main/java/com/drbhagwat/CoreJava/Chapter1/Exercise5.java
+++ CoreJava/src/main/java/com/drbhagwat/CoreJava/Chapter1/Exercise5.java
@@ -1,17 +1,25 @@
 package com.drbhagwat.CoreJava.Chapter1;
 
 
-/* 
- * What happens when you case a Double to an int that is larger than the 
- * largest possible int value. Try it out.
+/*
+ * Problem Statement:
+ *
+ * What happens when you cast a Double to an int that is larger than the largest possible int value. Try it out.
+ */
+
+/**
+ * This program prints when you cast a double (that is larger than the largest possible int) to an int.
+ *
+ * @author : Dinesh Bhagwat
+ * @version : 1.0
+ * @since : 2020-04-07
  */
 public class Exercise5 {
-
   public static void main(String[] args) {
-	int i =  (int) Double.MAX_VALUE;
-	
-	System.out.println("The largest double is " + Double.MAX_VALUE);
-	System.out.println("The value of an int variable, when a double that is larger than the largest possibel int is cast to it, is" + i);
-	System.out.println("The largest int value is " + Integer.MAX_VALUE);
+    int i = (int) Double.MAX_VALUE;
+
+    System.out.println("The largest double value is " + Double.MAX_VALUE);
+    System.out.println("The value of an int, when you cast a double larger than the largest possible int is " + i);
+    System.out.println("The largest int value is " + Integer.MAX_VALUE);
   }
 }
