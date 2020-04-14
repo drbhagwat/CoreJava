d9612db7659870bf579bc6b4429adc2c4ea066f6
bdinesh <bdinesh@s3groupinc.com>
2020-04-14 12:34:18 +0530

    Worked on Exercise9.

--- CoreJava/src/main/java/com/drbhagwat/CoreJava/Chapter1/Exercise8.java
+++ CoreJava/src/main/java/com/drbhagwat/CoreJava/Chapter1/Exercise8.java
@@ -1,48 +1,51 @@
 package com.drbhagwat.CoreJava.Chapter1;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 /*
- * Write a program that reads a string and prints all of its nonempty substrings.
+ * Problem Statement:
+ *
+ * Write a program that reads a string and prints all of its substrings.
+ */
+
+/**
+ * This program reads a string and prints all of its substrings.
+ *
+ * @author : Dinesh Bhagwat
+ * @version : 1.0
+ * @since : 2020-04-11
  */
 public class Exercise8 {
+  // define a static member variable to store the user input, so it can be in any of the helper methods.
+  private static String string;
+
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
     System.out.print("Please input a string: ");
-    String string = scanner.nextLine();
+    string = scanner.nextLine();
     scanner.close();
     int len = string.length();
-    System.out.println("All non-empty substrings of " + string + " follow:");
+    System.out.println("All substrings of " + string + " follow:");
 
     for (int i = 0; i <= len; i++) {
-      List<int[]> combinations = generate(len, i);
-
-      for (int[] combination : combinations) {
-        System.out.println(Arrays.toString(combination));
-      }
+      generate(len, i);
     }
   }
 
-  private static List<int[]> generate(int n, int r) {
-    List<int[]> combinations = new ArrayList<>();
-    helper(combinations, new int[r], 0, n - 1, 0);
-    return combinations;
+  private static void generate(int n, int r) {
+    helper(new char[r], 0, n - 1, 0);
   }
 
-  private static void helper(List<int[]> combinations, int data[], int start, int end, int index) {
-
+  private static void helper(char []data, int start, int end, int index) {
     if (index == data.length) {
-      int[] combination = data.clone();
-      combinations.add(combination);
+      System.out.println(Arrays.toString(data));
     } else {
 
       if (start <= end) {
-        data[index] = start;
-        helper(combinations, data, start + 1, end, index + 1);
-        helper(combinations, data, start + 1, end, index);
+        data[index] = string.charAt(start);
+        helper(data, start + 1, end, index + 1);
+        helper(data, start + 1, end, index);
       }
     }
   }
--- CoreJava/src/main/java/com/drbhagwat/CoreJava/Chapter1/Exercise9.java
+++ CoreJava/src/main/java/com/drbhagwat/CoreJava/Chapter1/Exercise9.java
@@ -1,39 +1,61 @@
 package com.drbhagwat.CoreJava.Chapter1;
 
-/* 
- * Section 1.5.3, "String comparison" (Page 25) has an example of two strings 
- * s and t so that s.equals(t) but s != t. Come up with a different example that
- * does not use substring.
+/*
+ * Problem Statement:
+ *
+ * Section 1.5.3, "String comparison" (Page 25) has an example of two strings
+ * s and t so that s.equals(t) but s != t. Come up with a different example
+ * that does not use substring.
+ */
+
+/**
+ * Section 1.5.3, "String comparison" (Page 25) has an example of two strings
+ * s and t so that s.equals(t) but s != t. Come up with a different example
+ * that does not use substring.
+ *
+ * @author : Dinesh Bhagwat
+ * @version : 1.0
+ * @since : 2020-04-14
  */
 public class Exercise9 {
   public static void main(String[] args) {
-	String string1 = "dinesh";
-	
-	/*
-	 * If you concatenate two or more string literals and if the result is exactly the same as 
-	 * an already defined string literal, Java is smart enough to point the reference of the resul to 
-	 * the earlier defined string literal as seen by the following example.
-	 */
-	
-	String string2 = "d" + "i" + "n" + "e" + "s" + "h";
-	
-	/*
-	 * Here, for string3, the memory is allocated on heap and hence though the content is the same as earlir, 
-	 * the reference is different.
-	*/
-	
-	String string3 = new String("dinesh");
+    String string1 = "dinesh";
 
-	/*
-	 * Once you internalize string3, the new string would again point to the same earlier defined 
-	 * string reference.
-	 */
+    /*
+     * If you concatenate two string literals and if the resulting content is
+     * the same as an already defined string literal, Java is smart enough to
+     * make the reference of the new string literal point to the string
+     * literal defined earlier - as seen by the following example.
+     */
 
-	String string4 = string3.intern();
+    String string2 = "d" + "i" + "n" + "e" + "s" + "h";
 
-	System.out.println("Is string1 == string2? " + string1.equals(string2));
-	System.out.println("Is string1 refers to the same address as string2? " + (string1 == string2));
-	System.out.println("Is string1 refers to the same address as string3? " + (string1 == string3));
-	System.out.println("Is string1 refers to the same address as string3? " + (string1 == string4));
+    System.out.println("Are the contents of string1,  string2 the same? " + string1.equals(string2));
+    System.out.println("Does string1 refer to the same memory location " +
+        "as string2? " + (string1 == string2));
+
+    /*
+     * Note that for string3, the memory is allocated on heap and hence though
+     * the content is the same as earlier, but, the reference is different.
+     */
+
+    String string3 = new String("dinesh");
+
+    System.out.println("Does string1 refer to the same address (memory " +
+        "location) as string3? " + (string1 == string3));
+
+    /*
+     * Once you internalize string3, the new string would again point to the
+     * same earlier defined string reference.
+     */
+
+    String string4 = string3.intern();
+
+    System.out.println("Does string4 refer to the same address as string1 " +
+        "after internalizing? "
+        + (string1 == string4));
+    System.out.println("Does string4 refer to the same address as string2 " +
+        "after internalizing? "
+        + (string2 == string4));
   }
 }
