1d630032a427485f193a5f4dffaf5b438c67b0b2
Dinesh Bhagwat <DBhagwat@dealersocket.com>
2021-06-01 16:53:43 +0530

    Worked on Chapter1 - Exercise3

--- src/main/java/com/drbhagwat/Chapter1/Exercise3.java
+++ src/main/java/com/drbhagwat/Chapter1/Exercise3.java
@@ -1,41 +1,58 @@
 package com.drbhagwat.Chapter1;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-/*
- * Problem Statement:
- *
+/**
+ * The following program is a solution to Exercise3 of Chapter1.
+ * <p>
  * Using only the conditional operator, write a program that reads three integers and prints the largest. Repeat with
  * Math.max.
- */
-
-/**
- * This program reads three integers and prints the largest. Repeat with Math.max.
  *
  * @author : Dinesh Bhagwat
  * @version : 1.0
- * @since : 2020-04-02
+ * @since : 2021-06-01
  */
 public class Exercise3 {
-  public static void main(String[] args) {
-    System.out.print("Please input 3 integers (on a single line, separated by white space or on three separate lines)" +
-				": ");
+    /**
+     * This method prompts the end-user to enter 3 integers (either on a single line, separated by white space
+     * or on three separate lines).
+     * If any integer entered is invalid, the method re-enters the while loop prompting for valid integers again.
+     * If any integer entered is valid, the method processes the integer for the desired output.
+     *
+     * @param args - command-line arguments (none).
+     * @throws InputMismatchException - throws this Exception, when an invalid integer is read.
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-    Scanner scanner = new Scanner(System.in);
-    int number1 = scanner.nextInt();
-    int number2 = scanner.nextInt();
-    int number3 = scanner.nextInt();
-    scanner.close();
+        while (true) {
+            System.out.print("Please input 3 integers (on a single line, separated by white space or " +
+                    "on three separate lines)" + ": ");
 
-    System.out.println("The three numbers are " + number1 + " " + number2 + " " + number3);
+            try {
+                int number1 = scanner.nextInt();
+                int number2 = scanner.nextInt();
+                int number3 = scanner.nextInt();
 
-    int greater = (number1 > number2) ? number1 : number2;
-    int greatest = (greater > number3) ? greater : number3;
-    System.out.println("The largest is " + greatest);
+                System.out.println("The three numbers are " + number1 + " " + number2 + " " + number3);
 
-    greater = Math.max(number1, number2);
-    greatest = Math.max(number3, greater);
+                int greater = (number1 > number2) ? number1 : number2;
+                int greatest = (greater > number3) ? greater : number3;
 
-    System.out.println("The largest is " + greatest);
-  }
+                System.out.println("The largest is " + greatest);
+
+                greater = Math.max(number1, number2);
+                greatest = Math.max(number3, greater);
+
+                System.out.println("The largest is " + greatest);
+
+                break;
+            } catch (InputMismatchException imE) {
+                scanner.nextLine(); // skip the new-line still present in the input steam
+            }
+        }
+        // As a good practice, always close the system resource (when not needed), as they are limited in number
+        scanner.close();
+    }
 }
