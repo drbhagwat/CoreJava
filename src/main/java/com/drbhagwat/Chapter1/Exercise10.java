f53b4f0df87a99cd5a35db20a90055bb90b78fa6
Dinesh Bhagwat <DBhagwat@dealersocket.com>
2021-06-08 15:49:31 +0530

    Completed Exerciese 10.

--- src/main/java/com/drbhagwat/Chapter1/Exercise10.java
+++ src/main/java/com/drbhagwat/Chapter1/Exercise10.java
@@ -2,24 +2,25 @@ package com.drbhagwat.Chapter1;
 
 import java.util.Random;
 
-/*
- * Problem Statement:
- *
- * Write a program that produces a random string of letters and digits by
- * generating a random long value and printing it in base 36.
- */
-
 /**
+ * The following program is a solution to Exercise10 of Chapter1.
+ * <p>
  * Write a program that produces a random string of letters and digits by
  * generating a random long value and printing it in base 36.
  *
  * @author : Dinesh Bhagwat
  * @version : 1.0
- * @since : 2020-04-14
+ * @since : 2021-06-08
  */
 public class Exercise10 {
-  public static void main(String[] args) {
-    System.out.println("A random string of letters and digits is " +
-        Long.toString(new Random().nextLong(), 36));
-  }
+    /**
+     * This method produces a random string of letters and digits by
+     * generating a random long value and printing it in base 36.
+     *
+     * @param args - command-line arguments (none).
+     */
+    public static void main(String[] args) {
+        System.out.println("A random string of letters and digits is " +
+                Long.toString(new Random().nextLong(), 36));
+    }
 }
