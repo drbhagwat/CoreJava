8f7ffb94b2cc0beedcdb419dda7293d81045ac08
Dinesh Bhagwat <DBhagwat@dealersocket.com>
2021-06-20 17:06:39 +0530

    Completed Exercise 3 of Chapter 1.

---
+++ src/main/java/com/drbhagwat/chapter/one/ExerciseThree.java
@@ -0,0 +1,60 @@
+package com.drbhagwat.chapter.one;
+
+import java.util.Scanner;
+import java.util.logging.Logger;
+
+/**
+ * The following program is a solution to Exercise 3 of Chapter 1.
+ * <p>
+ * Using only the conditional operator, write a program that reads three integers and prints the largest. Repeat with
+ * Math.max.
+ *
+ * @author : Dinesh Bhagwat
+ * @version : 1.0
+ * @since : 2021-June-20
+ */
+public class ExerciseThree {
+    /**
+     * This method prompts the end-user to enter 3 integers (either on a single line, separated by white space
+     * or on three separate lines).
+     * If any integer entered so is invalid, the method re-enters the while loop, prompting for 3 integers again.
+     * If all three integers entered are valid, the method processes them for the desired output.
+     *
+     * @param args - command-line arguments (none).
+     */
+    public static void main(String[] args) {
+        var logger = Logger.getLogger(Thread.currentThread().getClass().getName());
+        var scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("Please input 3 integers (either on a single line, separated by white space " +
+                    "or on three separate lines)" + ": ");
+
+            try {
+                var number1 = scanner.nextInt();
+                var number2 = scanner.nextInt();
+                var number3 = scanner.nextInt();
+                // As a good practice, always close the system resource (when not needed), as they are limited in number
+                scanner.close();
+
+                var message = "The three numbers are " + number1 + " " + number2 + " " + number3;
+                logger.info(message);
+
+                int greater = (number1 > number2) ? number1 : number2;
+                int greatest = (greater > number3) ? greater : number3;
+
+                message = "The largest is " + greatest;
+                logger.info(message);
+
+                greater = Math.max(number1, number2);
+                greatest = Math.max(number3, greater);
+
+                message = "The largest is " + greatest;
+                logger.info(message);
+                break;
+            } catch (Exception exception) { // catch the common exception, as multiple exceptions could be thrown
+                scanner.nextLine(); // skip the new-line present in the input steam
+            }
+        }
+    }
+}
