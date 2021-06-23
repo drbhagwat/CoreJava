7218d56eb250d2aaa85dd92121070f91d3674a25
Dinesh Bhagwat <DBhagwat@dealersocket.com>
2021-06-23 12:59:09 +0530

    Rephrased the questions

--- src/main/java/com/drbhagwat/chapter/one/ExerciseThree.java
+++ src/main/java/com/drbhagwat/chapter/one/ExerciseThree.java
@@ -14,9 +14,8 @@ import java.util.logging.Logger;
 public class ExerciseThree {
   /**
    * This method prompts the end-user to enter 3 integers (either on a single line, separated by white space
-   * or on three separate lines).
-   * If any integer entered is invalid, the method prompts for 3 integers again. Otherwise,
-   * the method uses only the conditional operator, prints the largest. It repeats the solution with Math.max.
+   * or on three separate lines). If any integer entered is invalid, it prompts for 3 integers again. Otherwise, it
+   * uses only the conditional operator, prints the largest. It repeats the solution with Math.max.
    *
    * @param args - command-line arguments (none).
    */
