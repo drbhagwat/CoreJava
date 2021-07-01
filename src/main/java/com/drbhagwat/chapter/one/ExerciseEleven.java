ce80249da589a4f6f41bc1d80c2d1068f6d36434
Dinesh Bhagwat <drbhagwat@yahoo.com>
2021-07-01 15:49:28 +0530

    Complted Exercise 11 of Chapter 1.

--- src/main/java/com/drbhagwat/chapter/one/ExerciseEleven.java
+++ src/main/java/com/drbhagwat/chapter/one/ExerciseEleven.java
@@ -11,34 +11,34 @@ import java.util.Scanner;
  * @since : 2021-July-01
  */
 public class ExerciseEleven {
-    /**
-     * This method reads a line of text and prints all the characters
-     * * that are not ASCII together with their Unicode values.
-     *
-     * @param args - command-line arguments (none).
-     */
-    public static void main(String[] args) {
-        System.out.print("Please input a string: ");
-        var scanner = new Scanner(System.in);
-        var string = scanner.nextLine();
-        scanner.close(); // close the system resource once done. System resources are limited in number * best practice
+  /**
+   * This method reads a line of text and prints all the characters that are not ASCII together with their Unicode
+   * values.
+   *
+   * @param args - command-line arguments (none).
+   */
+  public static void main(String[] args) {
+    System.out.print("Please input a string: ");
+    var scanner = new Scanner(System.in);
+    var string = scanner.nextLine();
+    scanner.close(); // close the system resource once done. System resources are limited in number * best practice
 
-        var length = string.codePointCount(0, string.length());
-        var i = 0;
+    var length = string.codePointCount(0, string.length());
+    var i = 0;
 
-        System.out.println("This program prints non-ASCII characters together " +
-                "with their Unicode values");
+    System.out.println("This program prints non-ASCII characters together " +
+        "with their Unicode values");
 
-        while (i < length) {
-            var j = string.offsetByCodePoints(i, 1);
-            var codePoint = string.substring(i, j);
-            var c = codePoint.charAt(0);
+    while (i < length) {
+      var j = string.offsetByCodePoints(i, 1);
+      var codePoint = string.substring(i, j);
+      var c = codePoint.charAt(0);
 
-            if (c > 127) {
-                System.out.println(codePoint);
-                System.out.printf("\\u%04x%n", (int) c);
-            }
-            i = j;
-        }
+      if (c > 127) {
+        System.out.println(codePoint);
+        System.out.printf("\\u%04x%n", (int) c);
+      }
+      i = j;
     }
+  }
 }
