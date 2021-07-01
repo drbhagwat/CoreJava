cd3925a8a7d81174f325680d98ec9e64e27e035a
Dinesh Bhagwat <drbhagwat@yahoo.com>
2021-07-01 16:09:56 +0530

    Complted Exercise 11 of Chapter 1.

--- src/main/java/com/drbhagwat/chapter/one/Exercise13.java
+++ src/main/java/com/drbhagwat/chapter/one/Exercise13.java
@@ -6,42 +6,39 @@ import java.util.List;
 import java.util.Random;
 
 /**
- * The following program is a solution to Exercise13 of Chapter1.
- * <p>
- * Write a program that prints a lottery combination, picking six distinct numbers between 1 and 49.
- * To pick six distinct numbers, start with an arrraylist filled with 1..49. Pick a random index and
- * remove the element. Repeat six times. Print the result in sorted order.
+ * Write a program that prints a lottery combination, picking six distinct numbers between 1 and 49. To pick six
+ * distinct numbers, start with an arraylist filled with 1..49. Pick a random index and remove the element. Repeat
+ * six times. Print the result in sorted order.
  *
  * @author : Dinesh Bhagwat
  * @version : 1.0
- * @since : 2021-06-12
+ * @since : 2021-July-01
  */
 public class Exercise13 {
-    /**
-     * This method prints a lottery combination, picking six distinct numbers between 1 and 49.
-     *
-     * @param args - command-line arguments (none).
-     */
-    public static void main(String[] args) {
-        final int TOTAL_NUMBER_OF_NUMBERS = 49;
-        final int NUMBERS_TO_PICK = 6;
+  /**
+   * This method prints a lottery combination, picking six distinct numbers between 1 and 49.
+   *
+   * @param args - command-line arguments (none).
+   */
+  public static void main(String[] args) {
+    final var TOTAL_NUMBER_OF_NUMBERS = 49;
+    final var NUMBERS_TO_PICK = 6;
 
-        List<Integer> listOfLotteryNumbers = new ArrayList<>(TOTAL_NUMBER_OF_NUMBERS);
+    List<Integer> listOfLotteryNumbers = new ArrayList<>();
 
-        for (int i = 0; i < TOTAL_NUMBER_OF_NUMBERS; i++) {
-            listOfLotteryNumbers.add(i);
-        }
-        List<Integer> pickedNumbers = new ArrayList<>();
-
-        for (int i = 0; i < NUMBERS_TO_PICK; i++) {
-            int randomIndex = new Random().nextInt(TOTAL_NUMBER_OF_NUMBERS);
-            pickedNumbers.add(listOfLotteryNumbers.remove(randomIndex));
-        }
-        pickedNumbers.sort(Comparator.comparingInt(i -> i));
-        int pickedNumbersSize = pickedNumbers.size();
-
-        for (int i = 0; i < pickedNumbersSize; i++) {
-            System.out.println(pickedNumbers.get(i));
-        }
+    for (var i = 0; i < TOTAL_NUMBER_OF_NUMBERS; i++) {
+      listOfLotteryNumbers.add(i);
     }
+    List<Integer> pickedNumbers = new ArrayList<>();
+
+    for (var i = 0; i < NUMBERS_TO_PICK; i++) {
+      pickedNumbers.add(listOfLotteryNumbers.get(new Random().nextInt(TOTAL_NUMBER_OF_NUMBERS)));
+    }
+    pickedNumbers.sort(Comparator.comparingInt(i -> i));
+    int pickedNumbersSize = pickedNumbers.size();
+
+    for (var i = 0; i < pickedNumbersSize; i++) {
+      System.out.println(pickedNumbers.get(i));
+    }
+  }
 }
