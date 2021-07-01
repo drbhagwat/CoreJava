09aad5d93ed65b153677d902d08cdc9a3faa446e
Dinesh Bhagwat <drbhagwat@yahoo.com>
2021-07-01 15:47:55 +0530

    Complted Exercise 11 of Cahpter 1.

--- src/main/java/com/drbhagwat/chapter/one/ExerciseEleven.java
+++ src/main/java/com/drbhagwat/chapter/one/ExerciseEleven.java
@@ -3,16 +3,14 @@ package com.drbhagwat.chapter.one;
 import java.util.Scanner;
 
 /**
- * The following program is a solution to Exercise11 of Chapter1.
- * <p>
  * Write a program that reads a line of text and prints all the characters
  * that are not ASCII together with their Unicode values.
  *
  * @author : Dinesh Bhagwat
  * @version : 1.0
- * @since : 2021-06-10
+ * @since : 2021-July-01
  */
-public class Exercise11 {
+public class ExerciseEleven {
     /**
      * This method reads a line of text and prints all the characters
      * * that are not ASCII together with their Unicode values.
@@ -21,24 +19,24 @@ public class Exercise11 {
      */
     public static void main(String[] args) {
         System.out.print("Please input a string: ");
-        Scanner scanner = new Scanner(System.in);
-        String string = scanner.nextLine();
-        scanner.close();
+        var scanner = new Scanner(System.in);
+        var string = scanner.nextLine();
+        scanner.close(); // close the system resource once done. System resources are limited in number * best practice
 
-        int length = string.codePointCount(0, string.length());
-        int i = 0;
+        var length = string.codePointCount(0, string.length());
+        var i = 0;
 
         System.out.println("This program prints non-ASCII characters together " +
                 "with their Unicode values");
 
         while (i < length) {
-            int j = string.offsetByCodePoints(i, 1);
-            String codePoint = string.substring(i, j);
-            char c = codePoint.charAt(0);
+            var j = string.offsetByCodePoints(i, 1);
+            var codePoint = string.substring(i, j);
+            var c = codePoint.charAt(0);
 
             if (c > 127) {
                 System.out.println(codePoint);
-                System.out.printf("\\u%04x\n", (int) c);
+                System.out.printf("\\u%04x%n", (int) c);
             }
             i = j;
         }
