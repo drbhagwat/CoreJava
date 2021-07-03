f733fbf848b43666767b0c59d61c9c8148713ecb
Dinesh Bhagwat <dbhagwat@dealersocket.com>
2021-07-03 11:38:39 +0530

    Completed Exercise 8 of Chapter 1.

--- src/main/java/com/drbhagwat/chapter/one/ExerciseEight.java
+++ src/main/java/com/drbhagwat/chapter/one/ExerciseEight.java
@@ -1,13 +1,14 @@
 package com.drbhagwat.chapter.one;
 
 import java.util.Scanner;
+import java.util.logging.Logger;
 
 /**
  * The following program reads a string and prints all of its substrings.
  *
  * @author : Dinesh Bhagwat
  * @version : 1.0
- * @since : 2021-June-28
+ * @since : 2021-July-03
  */
 public class ExerciseEight {
   /**
@@ -16,18 +17,25 @@ public class ExerciseEight {
    * @param args - command-line arguments (none).
    */
   public static void main(String[] args) {
-    Scanner  scanner = new Scanner(System.in);
-    System.out.print("Please input a string: ");
+    // get the name of the class in a generic way from the current thread instead of hardcoding
+    Logger logger = Logger.getLogger(Thread.currentThread().getClass().getName());
+    Scanner scanner = new Scanner(System.in);
+    logger.info("Please input a string: ");
     // define a final variable to store the user input.
     final String string = scanner.nextLine();
     scanner.close(); // close the system resource once done. System resources are limited in number * best practice
-    System.out.print("All substrings of " + string + " follow: ");
+    String message = "All substrings of " + string + " follow: ";
+    logger.info(message);
     printSubStrings(string);
   }
 
   private static void printSubStrings(final String string) {
+    // get the name of the class in a generic way from the current thread instead of hardcoding
+    Logger logger = Logger.getLogger(Thread.currentThread().getClass().getName());
+
     // finding the length of the string
     final int stringLength = string.length();
+    StringBuilder answer = new StringBuilder("");
 
     // selection of starting point
     for (int i = 0; i < stringLength; i++) {
@@ -37,15 +45,16 @@ public class ExerciseEight {
 
         // print from starting point to ending point
         for (int k = i; k <= j; k++) {
-          System.out.print(string.charAt(k));
+          answer.append(string.charAt(k));
         }
 
         if (i != stringLength - 1) {
-          System.out.print(',');
+          answer.append(',');
         }
       }
     }
-    System.out.println();
+    String message = String.valueOf(answer);
+    logger.info(message);
   }
 }
 
--- src/main/java/com/drbhagwat/chapter/one/ExerciseSeven.java
+++ src/main/java/com/drbhagwat/chapter/one/ExerciseSeven.java
@@ -10,12 +10,9 @@ import java.util.logging.Logger;
  *
  * @author : Dinesh Bhagwat
  * @version : 1.0
- * @since : 2021-July-02
+ * @since : 2021-July-03
  */
 public class ExerciseSeven {
-  private static final long LONG_VALUE_MIN = 0L;
-  private static final long LONG_VALUE_MAX = 4294967295L;
-
   /**
    * This method computes reads in two integers between 0 and 4294967295, stores them in int variables, and computes and
    * displays their unsigned sum, difference, product, quotient, and remainder. Do not convert them to long values.
@@ -25,48 +22,62 @@ public class ExerciseSeven {
   public static void main(String[] args) {
     // get the name of the class in a generic way from the current thread instead of hardcoding
     Logger logger = Logger.getLogger(Thread.currentThread().getClass().getName());
-    Scanner scanner = new Scanner(System.in);
     final String AND = " and ";
+    final long LONG_VALUE_MIN = 0L;
+    final long LONG_VALUE_MAX = 4294967295L;
+    Scanner scanner = new Scanner(System.in);
 
     while (true) {
       String message = "Please input two integers (each of them separated by white space, or on a separate line) - " +
           "both should lie between " + LONG_VALUE_MIN + AND + LONG_VALUE_MAX + " (both inclusive): ";
       logger.info(message);
+
       try {
         long longValue1 = scanner.nextLong();
         long longValue2 = scanner.nextLong();
+        boolean isFirstLongWithinRange = (longValue1 >= LONG_VALUE_MIN) && (longValue1 <= LONG_VALUE_MAX);
+        boolean isSecondLongWithinRange = (longValue2 >= LONG_VALUE_MIN) && (longValue2 <= LONG_VALUE_MAX);
 
-        if (longValue1 < LONG_VALUE_MIN || longValue1 > LONG_VALUE_MAX || longValue2 < LONG_VALUE_MIN || longValue2 > LONG_VALUE_MAX) {
-
-          if (longValue1 < LONG_VALUE_MIN || longValue1 > LONG_VALUE_MAX) {
-            message = "The first integer is not within the range " + LONG_VALUE_MIN + AND + LONG_VALUE_MAX;
-          } else {
-            message = "The second integer is not within the range " + LONG_VALUE_MIN + AND + LONG_VALUE_MAX;
-          }
+        while (!isFirstLongWithinRange) {
+          message =
+              "The first integer is not within the range " + LONG_VALUE_MIN + AND + LONG_VALUE_MAX + ". Please " +
+                  "re-enter it";
           logger.info(message);
           scanner.nextLine(); // move the scanner to the beginning of next line for an invalid input
-        } else {
-          scanner.close(); // close the system resource once done. System resources are limited in number * best practice
-          int intValue1 = (int) Integer.toUnsignedLong((int) longValue1);
-          int intValue2 = (int) Integer.toUnsignedLong((int) longValue2);
-
-          message = "The first integer is " + intValue1;
-          logger.info(message);
-          message = "The second integer is " + intValue2;
-          logger.info(message);
-          message = "Their sum is " + (intValue1 + intValue2);
-          logger.info(message);
-          message = "Their difference is " + (intValue1 - intValue2);
-          logger.info(message);
-          message = "Their product is " + intValue1 * intValue2;
-          logger.info(message);
-          message = "Their quotient is " + Integer.divideUnsigned(intValue1, intValue2);
-          logger.info(message);
-          message = "Their remainder is " + Integer.remainderUnsigned(intValue1, intValue2);
-          logger.info(message);
-          break; // break out of the while, as we successfully read an integer
+          longValue1 = scanner.nextLong();
+          isFirstLongWithinRange = (longValue1 >= LONG_VALUE_MIN) && (longValue1 <= LONG_VALUE_MAX);
         }
+
+        while (!isSecondLongWithinRange) {
+          message =
+              "The second integer is not within the range " + LONG_VALUE_MIN + AND + LONG_VALUE_MAX + ". Please re-enter it";
+          logger.info(message);
+          scanner.nextLine(); // move the scanner to the beginning of next line for an invalid input
+          longValue2 = scanner.nextLong();
+          isSecondLongWithinRange = (longValue2 >= LONG_VALUE_MIN) && (longValue2 <= LONG_VALUE_MAX);
+        }
+        scanner.close(); // close the system resource once done. System resources are limited in number * best practice
+        int intValue1 = (int) Integer.toUnsignedLong((int) longValue1);
+        int intValue2 = (int) Integer.toUnsignedLong((int) longValue2);
+
+        message = "The first integer is " + intValue1;
+        logger.info(message);
+        message = "The second integer is " + intValue2;
+        logger.info(message);
+        message = "Their sum is " + (intValue1 + intValue2);
+        logger.info(message);
+        message = "Their difference is " + (intValue1 - intValue2);
+        logger.info(message);
+        message = "Their product is " + intValue1 * intValue2;
+        logger.info(message);
+        message = "Their quotient is " + Integer.divideUnsigned(intValue1, intValue2);
+        logger.info(message);
+        message = "Their remainder is " + Integer.remainderUnsigned(intValue1, intValue2);
+        logger.info(message);
+        break; // break out of the while, as we successfully read an integer
       } catch (Exception exception) {
+        message = "Both integers are not within the range " + LONG_VALUE_MIN + AND + LONG_VALUE_MAX;
+        logger.info(message);
         scanner.nextLine();// move the scanner to the beginning of next line for an invalid input
       }
     }
